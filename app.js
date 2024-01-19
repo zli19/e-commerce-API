a32bdac126375c9b95cca92b2ee45aeda594a7f1
Quentin <zli19@rrc.ca>
2024-01-18 18:40:19 -0600

    add protection middlewares and api index

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 require('express-async-errors')
 
+
 // express
 const express = require('express')
 const app = express()
@@ -10,6 +11,10 @@ const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
+const rateLimiter = require('express-rate-limit')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const mongoSanitize = require('express-mongo-sanitize')
 
 // db
 const connectDB = require('./db/connect')
@@ -26,23 +31,27 @@ const notFound = require('./middleware/not-found')
 const errorHandler = require('./middleware/error-handler')
 
 // configure app
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 60, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+}))
 
-app.use(morgan('tiny'))
+
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+app.use(mongoSanitize())
+
+app.use(morgan('tiny')) //use in dev
 app.use(express.json())
 app.use(cookieParser(process.env.JWT_PRIVATE_KEY))
-app.use(cors())
 
 app.use(express.static('./public'))
 app.use(fileUpload())
 
-app.get('/', (req, res) => {
-    res.send('e-commerce api')
-})
-app.get('/api/v1', (req, res) => {
-    // console.log(req.cookies)
-    console.log(req.signedCookies)
-    res.send('e-commerce api')
-})
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/products', productRouter)
